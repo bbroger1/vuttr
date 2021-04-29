3734e4dc6ecd846a5fbadb8ff41b3b9d836c1bc6
Rogerio Carvalho <bb.roger.bb@gmail.com>
2021-04-28 22:06:19 -0300

    incluída autenticação com JWT

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,24 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('tools',  ['uses' => 'ToolController@index']);
+$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router) {
 
-$router->get('tools/{id}', ['uses' => 'ToolController@show']);
+    //routes para as ferramentas
+    $router->get('tools',  ['uses' => 'ToolController@index']);
+    $router->get('tools/{id}', ['uses' => 'ToolController@show']);
+    $router->get('tools/tag/{tag}', ['uses' => 'ToolController@showTag']);
+    $router->post('tools', ['uses' => 'ToolController@create']);
+    $router->delete('tools/{id}', ['uses' => 'ToolController@delete']);
+    $router->put('tools/{id}', ['uses' => 'ToolController@update']);
 
-$router->get('tools/tag/{tag}', ['uses' => 'ToolController@showTag']);
+    //routes para logout, dados do usuário autenticado e logout
+    $router->post('logout', 'AuthController@logout');
+    $router->patch('refresh', 'AuthController@refresh');
+    $router->post('me', 'AuthController@me');
+});
 
-$router->post('tools', ['uses' => 'ToolController@create']);
-
-$router->delete('tools/{id}', ['uses' => 'ToolController@delete']);
-
-$router->put('tools/{id}', ['uses' => 'ToolController@update']);
+$router->group(['prefix' => 'api'], function () use ($router) {
+    //routes para registro e autenticação de usuários
+    $router->post('register', 'AuthController@register');
+    $router->post('login', 'AuthController@login');
+});
